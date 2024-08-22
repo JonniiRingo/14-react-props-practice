4e316be87e978f2d51d1e609d35c724882e7ff36
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-22 16:23:29 -0700

    working on the seperation of concerns between the properties components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,16 @@ import contacts from "../commtacts";
 
 function App() {
   return (
-    <div className = "card">
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className = "name"> {contacts[0].name} </h2>
-          <img className = "circle-img" src = {contacts[0].imgURL}  alt="avatar_img" />
-        </div>
-        <div className="bottom">
-          <p className = "info"> {contacts[0].phone} </p> 
-          <p className = "info"> {contacts[0].email} </p>
-        </div>
-      </div>
+    <div>
+      <heading>My Contacts</heading>
+        <Card 
+          name = {contacts[0].name}
+          image = {contacts[0].imgURL}
+          phone = {contacts[0].phone}
+          email = {contacts[0].email}
+        />
     </div>
-  );
+  ); 
 }
 
 export default App;
